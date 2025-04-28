21e80afb54e0e30c7e9a7bcc478f9d105424b8d5
Leelaprasu95 <iprasupadma@gmail.com>
2025-04-28 16:43:10 +0530

    Create printNo.java

---
+++ printNo.java
@@ -0,0 +1,11 @@
+public class printNO {
+    public static void main(String[] args) {
+        System.out.println("27");
+        System.out.println("08");
+        System.out.println("1995");
+        System.out.print(10);
+        System.out.println(04);
+        System.out.print(2025);
+
+    }
+}
