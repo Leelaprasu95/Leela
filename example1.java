5f05125eb2c76820aec0841d7dacd27b1b9bdd36
Leelaprasu95 <iprasupadma@gmail.com>
2025-06-04 12:56:22 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,7 @@
+public class example1 {
+    public static void main(String[] args) {
+        System.out.print("My name is Leela\n");
+        System.err.print("I am going to learn Java");
+    }
+    
+}
